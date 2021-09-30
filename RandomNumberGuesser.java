0e5dc2e2038cdf2f19f19aa53abc6b5aa338ed68
DavidAbili <89999138+DavidAbili@users.noreply.github.com>
2021-09-30 17:10:22 -0400

    Add files via upload

---
+++ RandomNumberGuesser.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class RandomNumberGuesser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		//calls method
+		RNG.getCount();
+		RNG.rand();
+		//create and inializes variables
+		int random = RNG.rand();
+		int max = 100;
+		int min = 0;
+		String answer = "y";
+		//prompt user
+		System.out.println("Enter your first guess:");
+		//stores variable
+		int num = input.nextInt();
+		//determines what to print based on user
+		if(RNG.inputValidation(num,0,100)) {
+			System.out.println("Number of guesses " + RNG.getCount());
+			if(num > random) {
+				System.out.println("Your guess is to high");
+				max = num;
+			} else {
+				System.out.println("Your guess is to low");
+				min = num;
+			}
+		}
+		// loops until false
+		do {
+			//prompts user and shows range
+			System.out.println("Enter your next guess between " + min +" and " + max);
+		    // store variable
+			num = input.nextInt();
+			//determines what to proint based on user 
+			if(RNG.inputValidation(num,min,max)) {
+				System.out.println("Number of guesses " + RNG.getCount());
+				if(num > random) {
+					System.out.println("Your guess is to high");
+					max = num;
+				} else if(num < random) {
+					System.out.println("Your guess is to low");
+					min = num;
+				} else if(num==random) {
+					System.out.println("Congrats");
+					//prompts user for another round
+					System.out.println("Do you want to play again?(y or n)");
+					answer = input.nextLine();
+					answer = input.nextLine();
+				    // if true resets game and prompts user again
+					if(answer.equals("y")) {
+						min = 0;
+						max = 100;
+						RNG.resetCount();
+						random = RNG.rand();
+						System.out.println("Enter your first guess:");
+						num = input.nextInt();
+						if(num > random) {
+							System.out.println("Your guess is to high");
+							max = num;
+						} else if(num < random) {
+							System.out.println("Your guess is to low");
+							min = num;
+						}
+                         // if false ends the game
+					} else if(answer.equals("n")) {
+						System.out.println("Thanks For Playing");
+					}
+				}
+			}
+		}while(num != random || answer.equals("y"));
+
+
+
+	}
+
+}
